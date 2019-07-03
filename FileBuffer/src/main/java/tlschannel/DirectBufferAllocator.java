7db2316bdfa439c424bb5e6315c06d48d31af80f
spark3dev <spark.dev@gmail.com>
2019-07-03 21:06:39 +0900

    8th commit

---
+++ FileBuffer/src/main/java/tlschannel/DirectBufferAllocator.java
@@ -0,0 +1,31 @@
+package tlschannel;
+
+import java.nio.ByteBuffer;
+
+import engine.misc.DeallocationHelper;
+
+/**
+ * Allocator that creates direct buffers. The {@link #free(ByteBuffer)} method,
+ * if called, deallocates the buffer immediately, without having to wait for GC
+ * (and the finalizer) to run. Calling {@link #free(ByteBuffer)} is actually
+ * optional, but should result in reduced memory consumption.
+ * 
+ * Direct buffers are generally preferred for using with I/O, to avoid an extra
+ * user-space copy, or to reduce garbage collection overhead.
+ */
+public class DirectBufferAllocator implements BufferAllocator {
+
+    private DeallocationHelper deallocationHelper = new DeallocationHelper();
+
+    @Override
+    public ByteBuffer allocate(int size) {
+        return ByteBuffer.allocateDirect(size);
+    }
+
+    @Override
+    public void free(ByteBuffer buffer) {
+        // do not wait for GC (and finalizer) to run
+        deallocationHelper.deallocate(buffer);
+    }
+
+}
